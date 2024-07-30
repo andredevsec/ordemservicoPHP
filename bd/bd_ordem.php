ada5c64304a666ce1a0881a17249d5177e021b65
andredevsec <andre.all3@hotmail.com>
2024-07-30 19:04:58 -0300

    edita ordem quase pronto

--- bd/bd_ordem.php
+++ bd/bd_ordem.php
@@ -19,7 +19,7 @@ function listaOrdem() {
         array_push($ordens, $dados);
     }
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $ordens;
 }
     
@@ -32,35 +32,31 @@ function buscaOrdem($codigo) {
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_array($resultado);
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $dados;
 }
 
 function cadastraOrdem($cod_cliente, $cod_servico, $cod_terceirizado, $data_servico, $status, $data) {
     $conexao = conecta_bd();
 
-    // Verificar se o cod_servico existe na tabela servico
     $checkQuery = "SELECT cod FROM servico WHERE cod = '$cod_servico'";
     $resultado = mysqli_query($conexao, $checkQuery);
 
     if (mysqli_num_rows($resultado) > 0) {
-        // O valor existe, continue com a inserção
         $query = "INSERT INTO ordem (cod_cliente, cod_terceirizado, cod_servico, data_servico, status, data) VALUES ('$cod_cliente', '$cod_terceirizado', '$cod_servico', '$data_servico', '$status', '$data')";
         $resultado = mysqli_query($conexao, $query);
         $affectedRows = mysqli_affected_rows($conexao);
     } else {
-        // O valor não existe, exibe uma mensagem de erro 
         $affectedRows = "Error: cod_servico does not exist in servico table";
     }
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $affectedRows;
 }
 
 function buscaOrdemadd() {
     $conexao = conecta_bd();
 
-    // Ajuste a consulta para buscar a ordem mais recente, por exemplo:
     $query = "SELECT c.nome AS nome_cliente, t.nome AS nome_terceirizado, s.nome AS nome_servico, s.valor AS valor_servico, o.data_servico, o.status
               FROM ordem o
               JOIN cliente c ON o.cod_cliente = c.cod
@@ -97,22 +93,30 @@ function buscaOrdemeditar($codigo) {
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_array($resultado);
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $dados;
 }
 
-function editarOrdem($cod, $cod_cliente, $cod_terceiro, $cod_servico, $data_servico, $status, $data) {
+function editarOrdem($cod, $cod_cliente, $cod_terceirizado, $cod_servico, $data_servico, $status, $data) {
     $conexao = conecta_bd();
 
-    $query = "UPDATE ordem
-              SET cod_cliente = '$cod_cliente', cod_terceirizado = '$cod_terceiro', cod_servico = '$cod_servico', data_servico = '$data_servico', status = '$status', data = '$data'
-              WHERE cod = '$cod'";
-    mysqli_query($conexao, $query);
-    $affectedRows = mysqli_affected_rows($conexao);
+    $checkQuery = "SELECT cod FROM servico WHERE cod = '$cod_servico'";
+    $resultado = mysqli_query($conexao, $checkQuery);
 
-    mysqli_close($conexao); // Fechar a conexão
+    if (mysqli_num_rows($resultado) > 0) {
+        $query = "UPDATE ordem
+                  SET cod_cliente = '$cod_cliente', cod_terceirizado = '$cod_terceirizado', cod_servico = '$cod_servico', data_servico = '$data_servico', status = '$status', data = '$data'
+                  WHERE cod = '$cod'";
+        $resultado = mysqli_query($conexao, $query);
+        $affectedRows = mysqli_affected_rows($conexao);
+    } else {
+        $affectedRows = "Error: cod_servico does not exist in servico table";
+    }
+
+    mysqli_close($conexao);
     return $affectedRows;
 }
+
 
 function consultaStatusUsuario($codigo) {
     $conexao = conecta_bd();
@@ -120,7 +124,7 @@ function consultaStatusUsuario($codigo) {
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_assoc($resultado);
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $dados ? $dados : ['total' => 0];
 }
 
@@ -130,7 +134,7 @@ function consultaStatusCliente($codigo) {
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_assoc($resultado);
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $dados ? $dados : ['total' => 0];
 }
 
@@ -140,7 +144,7 @@ function consultaStatusTerceirizado($codigo) {
     $resultado = mysqli_query($conexao, $query);
     $dados = mysqli_fetch_assoc($resultado);
 
-    mysqli_close($conexao); // Fechar a conexão
+    mysqli_close($conexao);
     return $dados ? $dados : ['total' => 0];
 }
 
--- editar_ordem_envia.php
+++ editar_ordem_envia.php
@@ -3,12 +3,14 @@ require_once("valida_session.php");
 require_once ("bd/bd_ordem.php");
 	     
 $codigo = $_POST["cod"];
+$cod_cliente = $_POST["cod_cleinte"];
 $cod_terceirizado = $_POST["cod_terceirizado"];
+$cod_servico = $_POST["cod_servico"];
 $data_servico = $_POST["data_servico"];
 $status = $_POST["status"];
 $data=date("y/m/d");
 
-$dados = editarOrdem($codigo,$cod_terceirizado,$data_servico,$status,$data);
+$dados = editarOrdem($cod, $cod_cliente, $cod_terceirizado, $cod_servico, $data_servico, $status, $data) ;
 if ($dados == 1){
 	$_SESSION['texto_sucesso'] = 'Os dados da ordem de serviço foram alterados no sistema.';
 	header ("Location:ordem.php");
